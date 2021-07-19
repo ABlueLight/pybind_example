9f626fb4da213b9c65a7ffdebafbc104a270fb0d
ABlueLight <liujiangkuan.test@gmail.com>
2021-07-18 19:54:16 -0700

    add some code

--- 2_class_inherit/class_inherit.cpp
+++ 2_class_inherit/class_inherit.cpp
@@ -25,6 +25,43 @@ struct PolymorphicDog : PolymorphicPet {
     std::string bark() const { return "woof!"; }
 };
 
+
+class Animal {
+public:
+    virtual ~Animal() { }
+    virtual std::string go(int n_times) = 0;
+};
+
+class Bird : public Animal {
+public:
+    std::string go(int n_times) override {
+        std::string result;
+        for (int i=0; i<n_times; ++i)
+            result += "bee bee! ";
+        return result;
+    }
+};
+
+std::string call_go(Animal *animal) {
+    return animal->go(3);
+}
+
+//允许Python对Animal进行扩展
+class PyAnimal : public Animal {
+public:
+    using Animal::Animal;
+
+    std::string go(int n_times) override {
+        PYBIND11_OVERRIDE_PURE(
+            std::string, /* Return type */
+            Animal,      /* Parent class */
+            go,          /* Name of function in C++ (must match Python name) */
+            n_times      /* Argument(s) */
+        );
+    }
+};
+
+
 PYBIND11_MODULE(class_inherit, m)
 {
     py::class_<Pet>(m, "Pet")
@@ -56,6 +93,19 @@ PYBIND11_MODULE(class_inherit, m)
 
     // Again, return a base pointer to a derived instance, 
     m.def("pet_store2", []() { return std::unique_ptr<PolymorphicPet>(new PolymorphicDog); });
+
+    // py::class_<Animal>(m, "Animal")
+    //     .def("go", &Animal::go);
+
+    py::class_<Animal, PyAnimal>(m, "Animal")
+        .def(py::init<>())
+        .def("go", &Animal::go);
+
+
+    py::class_<Bird, Animal>(m, "Bird")
+        .def(py::init<>());
+        
+    m.def("call_go", &call_go);
 }
 
 // Method 2: pass parent class_ object:
--- 2_class_inherit/class_inherit.py
+++ 2_class_inherit/class_inherit.py
@@ -5,3 +5,15 @@ pet=Pet('ani')
 
 d.name
 pet.name
+
+
+b = Bird()
+call_go(b)
+
+#允许Python对Animal进行扩展
+class Cat(Animal):
+    def go(self, n_times):
+        return "meow! " * n_times
+
+c = Cat()
+call_go(c)
